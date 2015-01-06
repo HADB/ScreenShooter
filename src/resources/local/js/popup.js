b1f64fad67fdb7bbe83e646bd718aed4d761a84e
Bean <Bean.B.Deng@newegg.com>
2015-01-06 16:23:18 +0800

    移动文件位置

--- src/resources/local/js/popup.js
+++ src/resources/local/js/popup.js
@@ -1,7 +1,7 @@
 $(function () {
     $("#help").click(function () {
         chrome.tabs.create({
-            url: chrome.extension.getURL("") + "help.html"
+            url: chrome.extension.getURL("pages/help.html")
         });
         window.close();
     });
