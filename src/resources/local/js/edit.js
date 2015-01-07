9527743016ade351310a4cea0a9dc84e4db4e77b
Bean <Bean.B.Deng@newegg.com>
2015-01-07 14:56:19 +0800

    添加另存为图片的功能

--- src/resources/local/js/edit.js
+++ src/resources/local/js/edit.js
@@ -4,6 +4,10 @@ $(function () {
             setScreenshotImage(response.data);
         }
     });
+
+    $(".save-to-file").click(function () {
+        saveToFile();
+    });
 });
 
 function setScreenshotImage(data) {
@@ -17,4 +21,13 @@ function setScreenshotImage(data) {
         var context = canvas.getContext("2d");
         context.drawImage(image, 0, 0);
     };
+}
+
+function saveToFile() {
+    var link = document.createElement('a');
+    var date = new Date();
+    var dateStr = $.format.date(date, 'yyyyMMddHHmmss');
+    link.download = dateStr + ".png";
+    link.href = $("#screenshot")[0].toDataURL();
+    link.click();
 }
