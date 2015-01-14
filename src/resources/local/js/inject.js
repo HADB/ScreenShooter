50f5ca6e3138801a96c4ed54ec2f359b7d42a6d6
Bean <Bean.B.Deng@newegg.com>
2015-01-14 15:40:25 +0800

    修复选择区域截屏的bug

--- src/resources/local/js/inject.js
+++ src/resources/local/js/inject.js
@@ -11,14 +11,15 @@ screenShooterInject = {
     cropBottomAreaStart: { left: 0, width: 0, top: 0, height: 0 },
 };
 
-$(function () {
+(function () {
     showCropArea();
     bindCropEvents();
-});
+})(jQuery);
 
 function showCropArea() {
     $('body').append('<div class="crop-area crop-area-top"></div><div class="crop-area crop-area-bottom"></div><div class="crop-area crop-area-left"></div><div class="crop-area crop-area-right"></div><div class="crop-area crop-area-center"><div class="crop-button" style="display:none"><i class="fa fa-scissors"></i> 剪裁</div></div>');
     $('body').append('<div class="screen-mask"></div>');
+    $('.screen-mask').css('height', document.body.scrollHeight);
 }
 
 function bindCropEvents() {
@@ -35,10 +36,10 @@ function bindCropEvents() {
         screenShooterInject.isMouseDown = false;
         $('.crop-button').css('display', 'inline-block');
         console.log('mouseup');
-        var cropLeft = $('.crop-area-center').offset().left;
-        var cropTop = $('.crop-area-center').offset().top;
-        var cropWidth = $('.crop-area-center').width();
-        var cropHeight = $('.crop-area-center').height();
+        var cropLeft = $('.crop-area-center').offset().left - jQuery(document).scrollLeft() + 1;
+        var cropTop = $('.crop-area-center').offset().top - jQuery(document).scrollTop() + 1;
+        var cropWidth = $('.crop-area-center').width() - 1;
+        var cropHeight = $('.crop-area-center').height() - 1;
         var cropSettings = { left: cropLeft, top: cropTop, width: cropWidth, height: cropHeight };
         $('.crop-area').remove();
         $('.crop-button').remove();
@@ -50,8 +51,8 @@ function bindCropEvents() {
         if (screenShooterInject.isMouseDown && !screenShooterInject.cropCenterMouseDown) {
             console.log('move:(' + e.pageX + "," + e.pageY + ")");
             $('.screen-mask').css('opacity', 0);
-            var documentHeight = $('body').height();
-            var documentWidth = $('body').width();
+            var documentHeight = document.body.scrollHeight;
+            var documentWidth = document.body.scrollWidth;
             $('.crop-area-top').css('height', screenShooterInject.startY);
             $('.crop-area-bottom').css('top', e.pageY);
             $('.crop-area-bottom').css('height', documentHeight - e.pageY);
